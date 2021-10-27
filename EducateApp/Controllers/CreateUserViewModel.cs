45013ca24faf0ca9e8f6129a0b9ffe06aa7cc7b4
mffazy <mineev.filipp@mail.ru>
2021-10-27 11:16:16 +0500

    Добавление UsersController, управление пользователями

---
+++ EducateApp/Controllers/CreateUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EducateApp.Controllers
+{
+    public class CreateUserViewModel
+    {
+        public string Email { get; internal set; }
+        public string LastName { get; internal set; }
+        public string FirstName { get; internal set; }
+        public string Patronymic { get; internal set; }
+        public string Password { get; internal set; }
+    }
+}
